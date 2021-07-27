f37b2634d36605045ec30e744d313f40c919fbb8
msrecec <67606889+msrecec@users.noreply.github.com>
2021-07-27 16:23:15 +0200

    added on Start and on End events

--- src/app/game-control/game-control.component.ts
+++ src/app/game-control/game-control.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-game-control',
   templateUrl: './game-control.component.html',
-  styleUrls: ['./game-control.component.css']
+  styleUrls: ['./game-control.component.css'],
 })
 export class GameControlComponent implements OnInit {
+  @Output() start = new EventEmitter<number>();
+  buff?: number;
+  ref?: NodeJS.Timeout;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.buff = 0;
   }
 
+  /**
+   * Fire this event when starting the game
+   *
+   */
+
+  onStart() {
+    this.ref = setInterval((buffer = this.buff) => {
+      if (typeof buffer != 'undefined') {
+        buffer++;
+      } else {
+        console.error('Buffer was undefined, initializing on 0');
+        buffer = 0;
+      }
+      this.start.emit(buffer);
+    }, 1000);
+  }
+
+  /**
+   * Fire this event when ending the game
+   *
+   */
+
+  onEnd() {
+    if (typeof this.ref !== 'undefined') {
+      clearInterval(this.ref);
+    } else {
+      console.error('Reference is type of undefined');
+    }
+  }
 }
